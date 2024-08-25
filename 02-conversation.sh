61abfc11e1bc9cfedd139553c87256742242662a
Narahari <narahari517@gmail.com>
2024-08-25 22:34:13 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hari: Hi Naresh, how are you?"
+echo "Naresh: Hi Hari, I am fine, how are you?"
+echo "Hari: I am doing good, how was your holiday trip?"
+echo "Naresh: it went well. I wish you had come"
