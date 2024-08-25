0ed696457e06f1f79c02c2f9c456a16b8f048760
Narahari <narahari517@gmail.com>
2024-08-25 23:33:52 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ PERSON2=Malli
 
 
 echo "$PERSON1: Hi $PERSON2, how are you?"
-echo "$PERSON2: Hi H$PERSON1, I am fine, how are you?"
-echo "H$PERSON1: I am doing good, how was your holiday trip?"
+echo "$PERSON2: Hi $PERSON1, I am fine, how are you?"
+echo "$PERSON1: I am doing good, how was your holiday trip?"
 echo "$PERSON2: it went well. I wish you had come"
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter your user name"
+
+read USERNAME
+
+echo "Username entered is: $USERNAME"
