9c71d2a373bcca0b35a96c4f88abe275db79d9b6
Narahari <narahari517@gmail.com>
2024-08-25 22:51:41 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Hari
+PERSON2=Malli
+
+
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: Hi H$PERSON1, I am fine, how are you?"
+echo "H$PERSON1: I am doing good, how was your holiday trip?"
+echo "$PERSON2: it went well. I wish you had come"
