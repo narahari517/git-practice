d668658ff40bd4665919c0a5d93d1d5bfab95da7
Narahari <narahari517@gmail.com>
2024-08-25 23:25:57 +0530

    modify variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, how are you?"
+echo "$PERSON1: I am doing good, how was your holiday trip?"
+echo "$PERSON2: it went well. I wish you had come"
