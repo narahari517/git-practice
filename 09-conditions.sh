71b0bf5d74f94eec35239974d5e3ccbb56ade913
Narahari <narahari517@gmail.com>
2024-08-26 20:44:09 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,7 +3,7 @@
 Number=$1
 if [ $Number -gt 50 ]
 then 
-    echo "Given number is greater than 50"
+    echo "Given number: $Number is greater than 50"
 else
-    echo "Given number is less than 50"
+    echo "Given number: $Number is less than 50"
 fi
