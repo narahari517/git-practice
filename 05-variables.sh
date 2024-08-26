0ae3bc3a2b9dfb42f657e8707d131a182cbf9c44
Narahari <narahari517@gmail.com>
2024-08-26 11:43:50 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter your username"
+read -s USERNAME
+echo "Entered username is: $USERNAME"
+echo "Enter your password"
+read -s PASSWORD
+echo "Entered Password is: $PASSWORD"
+
