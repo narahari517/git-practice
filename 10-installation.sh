8085ae4883ece28531689c2b6bdbdc04275626d3
Narahari <narahari517@gmail.com>
2024-08-26 23:07:58 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -15,6 +15,29 @@ if [ $? -ne 0 ]
 then
     echo "git is not installed yet, installing now.."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not success, check it."
+        exit 1
+    else
+        echo "git installation is success"
+    fi
 else
     echo "git is already installed."
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed yet, installing now"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is not success, check it."
+    else
+        echo "mysql installation is success."
+    fi
+else
+    echo "mysql is already installed."
+fi    
