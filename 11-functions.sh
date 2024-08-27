8c91a60cce18a3d6b659d4f9be30a09f9f29af7a
Narahari <narahari517@gmail.com>
2024-08-27 12:09:22 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -21,36 +21,23 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing Git"
 
-# if [ $? -ne 0 ]
-# then
-#     echo "git is not installed yet, installing now.."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "git installation is not success, check it."
-#         exit 1
-#     else
-#         echo "git installation is success"
-#     fi
-# else
-#     echo "git is already installed."
-# fi
+if [ $? -ne 0 ]
+then
+    echo "git is not installed yet, installing now.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "git is already installed."
+fi
 
-# dnf list installed mysql
+dnf list installed mysql
 
-# if [ $? -ne 0 ]
-# then
-#     echo "mysql is not installed yet, installing now"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "mysql installation is not success, check it."
-#         exit 1
-#     else
-#         echo "mysql installation is success."
-#     fi
-# else
-#     echo "mysql is already installed."
-# fi    
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed yet, installing now"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "mysql is already installed."
+fi
