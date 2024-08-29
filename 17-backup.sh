251c5c664c763bfbd0c1e8b73b30c6efe1aae480
Narahari <narahari517@gmail.com>
2024-08-29 22:52:44 +0530

    logs_delete

--- 17-backup.sh
+++ 17-backup.sh
@@ -20,3 +20,29 @@ if [ $# -lt 2 ]
 then
     USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR does not exist, please check"
+    exit 1
+fi
+
+if [ ! -d $DESTINATION_DIR ]
+then
+    echo "$DESTINATION_DIR does not exist, please check"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime $DAYS)
+
+echo "files:$FILES"
+
+if [ ! -z $FILES ] #true if files is empty
+then
+    echo "files are found"
+else
+    echo "no files older than $DAYS"
+fi
+
+
+
